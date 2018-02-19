aa325de0ea84dc340ea0c38e2934896693ca7039
djkonro <konraddjimeli@gmail.com>
2018-02-20 00:42:51 +0100

    [WIP] Add unit tests for functest-kubernetes
    Change-Id: I4c9b5a56e891f4a63c86e0d7c4dd0d02de677610
    Signed-off-by: djkonro <konraddjimeli@gmail.com>

---
+++ functest_kubernetes/test_k8stest.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2018 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+"""Define the classes required to fully cover k8s."""
+
+import logging
+import os
+import unittest
+
+import mock
+
+from functest.core import testcase
+from functest_kubernetes import k8stest
+
+
+class K8sTests(unittest.TestCase):
+
+    def setUp(self):
+        os.environ["DEPLOY_SCENARIO"] = "k8-test"
+        os.environ["KUBECONFIG"] = "/home/config"
+        os.environ["KUBE_MASTER_IP"] = "127.0.0.1"
+        os.environ["KUBE_MASTER_URL"] = "https://127.0.0.1:6443"
+        os.environ["KUBERNETES_PROVIDER"] = "local"
+
+    def _test_no_env_var(self, var):
+        del os.environ[var]
+        with self.assertRaises(Exception):
+            k8stest.K8sTesting().check_envs()
+
+    def test_no_deploy_scenario(self):
+        self._test_no_env_var("DEPLOY_SCENARIO")
+
+    def test_no_kubeconfig(self):
+        self._test_no_env_var("KUBECONFIG")
+
+    def test_no_kube_master_ip(self):
+        self._test_no_env_var("KUBE_MASTER_IP")
+
+    def test_no_kube_master_url(self):
+        self._test_no_env_var("KUBE_MASTER_URL")
+
+    def test_no_kubernetes_provider(self):
+        self._test_no_env_var("KUBERNETES_PROVIDER")
+
+
+if __name__ == "__main__":
+    logging.disable(logging.CRITICAL)
+    unittest.main(verbosity=2)
