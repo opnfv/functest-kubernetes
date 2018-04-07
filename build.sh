13ac225eb6afbd4e81109ead35d0594c53ce888b
Cédric Ollivier <cedric.ollivier@orange.com>
2018-04-07 11:03:34 +0200

    Split opnfv/functest-kubernetes
    4 containers are now proposed
      - opnfv/functest-kubernetes-core
      - opnfv/functest-kubernetes-healthcheck
      - opnfv/functest-kubernetes-smoke
      - opnfv/functest-kubernetes-features
    
    Change-Id: I7d6f1edc7e9ace185b2cf991592ebc841f5520b7
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+repo=${REPO:-opnfv}
+amd64_dirs=${amd64_dirs-"\
+docker/core \
+docker/healthcheck \
+docker/smoke \
+docker/features"}
+build_opts=(--pull=true --no-cache --force-rm=true)
+
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-kubernetes-core|${repo}/functest-kubernetes-core|g" {} +
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-kubernetes-healthcheck|${repo}/functest-kubernetes-healthcheck|g" {} +
+for dir in ${amd64_dirs}; do
+    (cd "${dir}" && docker build "${build_opts[@]}" -t "${repo}/functest-kubernetes-${dir##**/}" .)
+    docker push "${repo}/functest-kubernetes-${dir##**/}"
+    [ "${dir}" != "docker/core" ] && (docker rmi "${repo}/functest-kubernetes-${dir##**/}" || true)
+done
+[ ! -z "${amd64_dirs}" ] && (docker rmi "${repo}/functest-kubernetes-core:latest" alpine:3.7 || true)
+find . -name Dockerfile -exec git checkout {} +
+
+exit $?
