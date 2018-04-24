c3e48ccef64acc6ba31823e1c8d6138eebbffb8c
Cédric Ollivier <cedric.ollivier@orange.com>
2018-04-24 19:34:54 +0200

    Check build.sh via bashate
    Change-Id: I367d87539ce25d9d114ee1f75b2f8af383408865
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit f351c301be1396e5dd578209c606a111afbc8758)

--- build.sh
+++ build.sh
@@ -10,14 +10,22 @@ docker/smoke \
 docker/features"}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
-find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-kubernetes-core:fraser|${repo}/functest-kubernetes-core:fraser|g" {} +
-find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-kubernetes-healthcheck:fraser|${repo}/functest-kubernetes-healthcheck:fraser|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-kubernetes-core:fraser|\
+${repo}/functest-kubernetes-core:fraser|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-kubernetes-healthcheck:fraser|\
+${repo}/functest-kubernetes-healthcheck:fraser|g" {} +
 for dir in ${amd64_dirs}; do
-    (cd "${dir}" && docker build "${build_opts[@]}" -t "${repo}/functest-kubernetes-${dir##**/}:fraser" .)
+    (cd "${dir}" && docker build "${build_opts[@]}" \
+        -t "${repo}/functest-kubernetes-${dir##**/}:fraser" .)
     docker push "${repo}/functest-kubernetes-${dir##**/}:fraser"
-    [ "${dir}" != "docker/core" ] && (docker rmi "${repo}/functest-kubernetes-${dir##**/}:fraser" || true)
+    [ "${dir}" != "docker/core" ] &&
+        (docker rmi "${repo}/functest-kubernetes-${dir##**/}:fraser" || true)
 done
-[ ! -z "${amd64_dirs}" ] && (docker rmi "${repo}/functest-kubernetes-healthcheck:fraser" "${repo}/functest-kubernetes-core:fraser" alpine:3.7 || true)
+[ ! -z "${amd64_dirs}" ] &&
+    (docker rmi "${repo}/functest-kubernetes-healthcheck:fraser" \
+        "${repo}/functest-kubernetes-core:fraser" alpine:3.7 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 exit $?
