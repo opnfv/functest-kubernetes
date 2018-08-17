556060ac48277eac2c44a1094ce620d4f2a1a3fa
Jingzhao Ni <jingzhao.ni@arm.com>
2018-08-17 16:58:58 +0800

    Add some build option in build.sh for supporting arm arch
    In build.sh, add some build options for arm arch.
    
    Change-Id: I3cc6c10e2e52c281e2324d8b42b721383be30c0b
    Signed-off-by: Jingzhao Ni <jingzhao.ni@arm.com>

--- build.sh
+++ build.sh
@@ -8,6 +8,8 @@ docker/core \
 docker/healthcheck \
 docker/smoke \
 docker/features"}
+arm64_dirs=${arm64_dirs-${amd64_dirs}}
+
 build_opts=(--pull=true --no-cache --force-rm=true)
 
 find . -name Dockerfile -exec sed -i \
@@ -27,5 +29,29 @@ done
     (docker rmi "${repo}/functest-kubernetes-healthcheck" \
         "${repo}/functest-kubernetes-core" alpine:3.7 || true)
 find . -name Dockerfile -exec git checkout {} +
+
+
+#for arm-arch
+find . -name Dockerfile -exec sed -i \
+    -e "s|alpine:3.7|multiarch/alpine:arm64-v3.7|g" {} +
+
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-kubernetes-core|${repo}/functest-kubernetes-core|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-kubernetes-healthcheck|${repo}/functest-kubernetes-healthcheck|g" {} +
+for dir in ${arm64_dirs}; do
+    (cd "${dir}" && docker build "${build_opts[@]}" \
+        -t "${repo}/functest-kubernetes-${dir##**/}" .)
+    ## docker push "${repo}/functest-kubernetes-${dir##**/}"
+
+    [ "${dir}" != "docker/core" ] &&
+        (docker rmi "${repo}/functest-kubernetes-${dir##**/}" || true)
+done
+[ ! -z "${arm64_dirs}" ] &&
+    (docker rmi "${repo}/functest-kubernetes-healthcheck" \
+        "${repo}/functest-kubernetes-core" multiarch/alpine:arm64-v3.7 || true)
+find . -name Dockerfile -exec git checkout {} +
+
+
 
 exit $?
