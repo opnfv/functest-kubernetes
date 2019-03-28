03fda103435bdfa93c6438b7013cffe4447a94ca
Cédric Ollivier <cedric.ollivier@orange.com>
2019-03-28 07:35:28 +0100

    Publish Functest Kubernetes Iruya
    Change-Id: I7ea9454f63b403f140961291a4dd14c563e2a82c
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -12,44 +12,44 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
-${repo}/functest-kubernetes-core:amd64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-core:iruya|\
+${repo}/functest-kubernetes-core:amd64-iruya:iruya|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
-${repo}/functest-kubernetes-healthcheck:amd64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-healthcheck:iruya|\
+${repo}/functest-kubernetes-healthcheck:amd64-iruya|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
-            -t "${repo}/functest-kubernetes-${dir##**/}:amd64-latest" .)
-        docker push "${repo}/functest-kubernetes-${dir##**/}:amd64-latest"
+            -t "${repo}/functest-kubernetes-${dir##**/}:amd64-iruya" .)
+        docker push "${repo}/functest-kubernetes-${dir##**/}:amd64-iruya"
     [ "${dir}" != "docker/core" ] &&
         (docker rmi \
-            "${repo}/functest-kubernetes-${dir##**/}:amd64-latest" || true)
+            "${repo}/functest-kubernetes-${dir##**/}:amd64-iruya" || true)
 done
 [ ! -z "${amd64_dirs}" ] &&
     (docker rmi \
-        "${repo}/functest-kubernetes-core:amd64-latest" golang:alpine3.9 || true)
+        "${repo}/functest-kubernetes-core:amd64-iruya" golang:1.12.1-alpine3.9 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|golang:alpine3.9|arm64v8/golang:alpine3.9|g" {} +
+    -e "s|golang:1.12.1-alpine3.9|arm64v8/golang:1.12.1-alpine3.9|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
-${repo}/functest-kubernetes-core:arm64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-core:iruya|\
+${repo}/functest-kubernetes-core:arm64-iruya|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
-${repo}/functest-kubernetes-healthcheck:arm64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-healthcheck:iruya|\
+${repo}/functest-kubernetes-healthcheck:arm64-iruya|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-kubernetes-${dir##**/}:arm64-latest" .)
-    docker push "${repo}/functest-kubernetes-${dir##**/}:arm64-latest"
+        -t "${repo}/functest-kubernetes-${dir##**/}:arm64-iruya" .)
+    docker push "${repo}/functest-kubernetes-${dir##**/}:arm64-iruya"
     [ "${dir}" != "docker/core" ] &&
         (docker rmi \
-            "${repo}/functest-kubernetes-${dir##**/}:arm64-latest" || true)
+            "${repo}/functest-kubernetes-${dir##**/}:arm64-iruya" || true)
 done
 [ ! -z "${arm64_dirs}" ] &&
-    (docker rmi "${repo}/functest-kubernetes-core:arm64-latest" \
-        arm64v8/golang:alpine3.9 || true)
+    (docker rmi "${repo}/functest-kubernetes-core:arm64-iruya" \
+        arm64v8/golang:1.11.6-alpine3.9 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 exit $?
