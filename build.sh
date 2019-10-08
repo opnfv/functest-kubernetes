9f1d167b0356c5e3fb58edc0ef1e1f9623dcf857
Cédric Ollivier <cedric.ollivier@orange.com>
2019-10-08 06:29:36 +0200

    Fix sed calls in build.sh
    Change-Id: I880c49c1f56256813e400076d49a49de9b1b0203
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -11,10 +11,10 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
+    -e "s|opnfv/functest-kubernetes-core:jerma|\
 ${repo}/functest-kubernetes-core:amd64-jerma|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
+    -e "s|opnfv/functest-kubernetes-healthcheck:jerma|\
 ${repo}/functest-kubernetes-healthcheck:amd64-jerma|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
@@ -34,10 +34,10 @@ find . -name Dockerfile -exec git checkout {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|golang:1.12-alpine3.10|arm64v8/golang:1.12-alpine3.10|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
+    -e "s|opnfv/functest-kubernetes-core:jerma|\
 ${repo}/functest-kubernetes-core:arm64-jerma|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
+    -e "s|opnfv/functest-kubernetes-healthcheck:jerma|\
 ${repo}/functest-kubernetes-healthcheck:arm64-jerma|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
