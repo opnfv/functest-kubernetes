0da18ecfd9db456545171197796614cae7d553fc
Cédric Ollivier <cedric.ollivier@orange.com>
2020-08-10 21:40:11 +0200

    Compile all Functest K8s containers for arm v7 and v8
    It removes sudo when useless and turns debug on when compiling K8s
    binaries. It avoids TravisCI timeouts (No output has been received)[1]
    
    [1] https://travis-ci.org/github/collivier/functest-kubernetes/jobs/716819529
    
    Change-Id: I4719257596bf9a6510e2f772271b336847d1ccb5
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit 3c4543a8ef6f84a5f25483a6dfe50e0075a9cd0e)

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ docker/healthcheck \
 docker/smoke \
 docker/cnf \
 docker/security"}
+arm_dirs=${arm_dirs-${amd64_dirs}}
 arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
@@ -52,6 +53,27 @@ done
 [ ! -z "${arm64_dirs}" ] &&
     (docker rmi "${repo}/functest-kubernetes-core:arm64-iruya" \
         arm64v8/golang:1.12-alpine3.9 || true)
+find . -name Dockerfile -exec git checkout {} +
+
+find . -name Dockerfile -exec sed -i \
+    -e "s|golang:1.14-alpine3.12|arm32v7/golang:1.14-alpine3.12|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-kubernetes-core|\
+${repo}/functest-kubernetes-core:arm-iruya|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-kubernetes-healthcheck|\
+${repo}/functest-kubernetes-healthcheck:arm-iruya|g" {} +
+for dir in ${arm_dirs}; do
+    (cd "${dir}" && docker build "${build_opts[@]}" \
+        -t "${repo}/functest-kubernetes-${dir##**/}:arm-iruya" .)
+    docker push "${repo}/functest-kubernetes-${dir##**/}:arm-iruya"
+    [ "${dir}" != "docker/core" ] &&
+        (docker rmi \
+            "${repo}/functest-kubernetes-${dir##**/}:arm-iruya" || true)
+done
+[ ! -z "${arm_dirs}" ] &&
+    (docker rmi "${repo}/functest-kubernetes-core:arm-iruya" \
+        arm32v7/golang:1.14-alpine3.12 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 exit $?
