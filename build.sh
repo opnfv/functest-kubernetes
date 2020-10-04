d8d73dabb79482d5f8fc7a2d8d4522ed073c5e17
Cédric Ollivier <cedric.ollivier@orange.com>
2020-10-04 12:54:01 +0200

    Prepare Functest Kubernetes Leguer containers
    Change-Id: I91b31f2af0c2265cdda5715ae152558ffcbe003b
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -15,71 +15,71 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
-${repo}/functest-kubernetes-core:amd64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-core:leguer|\
+${repo}/functest-kubernetes-core:amd64-leguer|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
-${repo}/functest-kubernetes-healthcheck:amd64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-healthcheck:leguer|\
+${repo}/functest-kubernetes-healthcheck:amd64-leguer|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
-            -t "${repo}/functest-kubernetes-${dir##**/}:amd64-latest" .)
-        docker push "${repo}/functest-kubernetes-${dir##**/}:amd64-latest"
+            -t "${repo}/functest-kubernetes-${dir##**/}:amd64-leguer" .)
+        docker push "${repo}/functest-kubernetes-${dir##**/}:amd64-leguer"
     [ "${dir}" != "docker/core" ] &&
         (docker rmi \
-            "${repo}/functest-kubernetes-${dir##**/}:amd64-latest" || true)
+            "${repo}/functest-kubernetes-${dir##**/}:amd64-leguer" || true)
 done
 [ ! -z "${amd64_dirs}" ] &&
     (docker rmi \
-        "${repo}/functest-kubernetes-core:amd64-latest" \
+        "${repo}/functest-kubernetes-core:amd64-leguer" \
         alpine:3.12 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.12|arm64v8/alpine:3.12|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
-${repo}/functest-kubernetes-core:arm64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-core:leguer|\
+${repo}/functest-kubernetes-core:arm64-leguer|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
-${repo}/functest-kubernetes-healthcheck:arm64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-healthcheck:leguer|\
+${repo}/functest-kubernetes-healthcheck:arm64-leguer|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-smoke|\
-${repo}/functest-kubernetes-smoke:arm64-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-smoke:leguer|\
+${repo}/functest-kubernetes-smoke:arm64-leguer|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-kubernetes-${dir##**/}:arm64-latest" .)
-    docker push "${repo}/functest-kubernetes-${dir##**/}:arm64-latest"
+        -t "${repo}/functest-kubernetes-${dir##**/}:arm64-leguer" .)
+    docker push "${repo}/functest-kubernetes-${dir##**/}:arm64-leguer"
     [ "${dir}" != "docker/core" ] &&
         (docker rmi \
-            "${repo}/functest-kubernetes-${dir##**/}:arm64-latest" || true)
+            "${repo}/functest-kubernetes-${dir##**/}:arm64-leguer" || true)
 done
 [ ! -z "${arm64_dirs}" ] &&
-    (docker rmi "${repo}/functest-kubernetes-core:arm64-latest" \
+    (docker rmi "${repo}/functest-kubernetes-core:arm64-leguer" \
         arm64v8/alpine:3.12 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.12|arm32v7/alpine:3.12|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-core|\
-${repo}/functest-kubernetes-core:arm-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-core:leguer|\
+${repo}/functest-kubernetes-core:arm-leguer|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-healthcheck|\
-${repo}/functest-kubernetes-healthcheck:arm-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-healthcheck:leguer|\
+${repo}/functest-kubernetes-healthcheck:arm-leguer|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-kubernetes-smoke|\
-${repo}/functest-kubernetes-smoke:arm-latest|g" {} +
+    -e "s|opnfv/functest-kubernetes-smoke:leguer|\
+${repo}/functest-kubernetes-smoke:arm-leguer|g" {} +
 for dir in ${arm_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-kubernetes-${dir##**/}:arm-latest" .)
-    docker push "${repo}/functest-kubernetes-${dir##**/}:arm-latest"
+        -t "${repo}/functest-kubernetes-${dir##**/}:arm-leguer" .)
+    docker push "${repo}/functest-kubernetes-${dir##**/}:arm-leguer"
     [ "${dir}" != "docker/core" ] &&
         (docker rmi \
-            "${repo}/functest-kubernetes-${dir##**/}:arm-latest" || true)
+            "${repo}/functest-kubernetes-${dir##**/}:arm-leguer" || true)
 done
 [ ! -z "${arm_dirs}" ] &&
-    (docker rmi "${repo}/functest-kubernetes-core:arm-latest" \
+    (docker rmi "${repo}/functest-kubernetes-core:arm-leguer" \
         arm32v7/alpine:3.12 || true)
 find . -name Dockerfile -exec git checkout {} +
 
